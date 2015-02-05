bf3b0581ce05215283d9947efbe1265e8102cbae
Rosie Le Faive <lefaive@gmail.com>
2015-02-05 10:21:44 -0400

    When attaching js with Behaviours, put context in selector following best practices.

--- modules/js/initialize.js
+++ modules/js/initialize.js
@@ -2,24 +2,24 @@
 
     Drupal.behaviors.myModuleBehavior = {
         attach: function (context, settings) {
-            var audio_element = $('.audiojs-global');
+            var audio_element = $('.audiojs-global', context);
             var a = audiojs.createAll({
                 trackEnded: function() {
-                  $('.audiojs-global-link.playing').removeClass('playing');
+                  $('.audiojs-global-link.playing', context).removeClass('playing');
                 }
             }, audio_element);
 
             // TODO: Add next.
             var audio = a[0];
-            var now_playing = $('.now-playing');
-            $('.audiojs-global-link').click(function(e) {
+            var now_playing = $('.now-playing', context);
+            $('.audiojs-global-link', context).click(function(e) {
                 e.preventDefault();
                 if ($(this).hasClass('playing')) {
                     audio.pause();
                     $(this).removeClass('playing');
                 }
                 else {
-                    $('.audiojs-global-link').removeClass('playing');
+                    $('.audiojs-global-link', context).removeClass('playing');
                     $(this).addClass('playing');
                     audio.load($(this).attr('data-source'));
                     audio.play();
